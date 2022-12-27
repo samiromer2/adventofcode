8ea88a37e00e754c685b496deb433fac450cadbf
Samir Omer <samiromer2@yahoo.com>
2022-12-27 21:54:58 +0200

    beauty code

--- php/Day1/day1.php
+++ php/Day1/day1.php
@@ -4,22 +4,19 @@ $counterB = 0;
 $firstbase = 0;
 $myfile = fopen("input.txt", "r") or die("Unable to open file!");
 // Output one character until end-of-file
-while(!feof($myfile)) {
+while (!feof($myfile)) {
     $counterB++;
     $singlechar = fgetc($myfile);
-    if ($singlechar =='(')
-    $floor++;
-    if ($singlechar ==')')
-    $floor--;
-    if ($floor == -1 && $firstbase == 0 )
-    {
-        $firstbase = $counterB;  
+    if ($singlechar == '(')
+        $floor++;
+    if ($singlechar == ')')
+        $floor--;
+    if ($floor == -1 && $firstbase == 0) {
+        $firstbase = $counterB;
         echo "basement ";
     }
-        
 }
 fclose($myfile);
 echo $floor;
 echo "<br>";
-echo "first basement".$firstbase;
-?> 
+echo "first basement" . $firstbase;

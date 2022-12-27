12b43d40905f2c356661a19be772ffab27b37ef6
Samir Omer <samiromer2@yahoo.com>
2022-12-27 21:46:54 +0200

    day one of 2015 the 2 halfs

---
+++ php/Day1/day1.php
@@ -0,0 +1,25 @@
+<?php
+$floor = 0;
+$counterB = 0;
+$firstbase = 0;
+$myfile = fopen("input.txt", "r") or die("Unable to open file!");
+// Output one character until end-of-file
+while(!feof($myfile)) {
+    $counterB++;
+    $singlechar = fgetc($myfile);
+    if ($singlechar =='(')
+    $floor++;
+    if ($singlechar ==')')
+    $floor--;
+    if ($floor == -1 && $firstbase == 0 )
+    {
+        $firstbase = $counterB;  
+        echo "basement ";
+    }
+        
+}
+fclose($myfile);
+echo $floor;
+echo "<br>";
+echo "first basement".$firstbase;
+?> 

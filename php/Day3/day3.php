d43ae927ba158dce0d48acd8ad9de24f082c833c
Samir Omer <samiromer2@yahoo.com>
2022-12-29 03:34:52 +0200

    day 3 part 2 done

--- php/Day3/day3.php
+++ php/Day3/day3.php
@@ -1,27 +1,55 @@
 <?php
-$house = 0;
-
+$position      = [0, 0];
+$positionrobot = [0, 0];
+$counter = 0;
+$visited[]="0-0";
 $myfile = fopen("input.txt", "r") or die("Unable to open file!");
 // Output one character until end-of-file
 while (!feof($myfile)) {
-     $singlechar = fgetc($myfile);
-    switch ($singlechar) {
-        case '^':
-            echo "north";
-            break;
-        case 'v':
-            echo "south";
-            break;
-        case '>':
-            echo "east";
-            break;
-        case '<':
-            echo "west";
-            break;
-
-        default:
-            echo "Weird";
-            break;
+    $singlechar = fgetc($myfile);
+    if ($counter%2 != 0) {
+        switch ($singlechar) {
+            case '^':
+                //$position = [$position[0], $position[1] + 1];
+                $position[1]=$position[1] + 1;
+                break;
+            case 'v':
+                $position[1] =  $position[1] - 1;
+                break;
+            case '>':
+                $position[0] = $position[0] + 1;
+                break;
+            case '<':
+                $position[0] = $position[0] - 1;
+                break;
+        }
+        
+         $visited[] = $position[0] . "-" . $position[1];
+       
     }
+    if ($counter%2 == 0) {
+        switch ($singlechar) {
+            case '^':
+                $positionrobot[1] =  intval($positionrobot[1]) + 1;
+                break;
+            case 'v':
+                $positionrobot[1] =  intval($positionrobot[1]) - 1;
+                break;
+            case '>':
+                $positionrobot[0] = intval($positionrobot[0]) + 1;
+                break;
+            case '<':
+                $positionrobot[0] = intval($positionrobot[0]) - 1;
+                break;
+        }
+         $visited[] = $positionrobot[0]."-".$positionrobot[1];
+       
+    }
+     $counter ++;
 }
 fclose($myfile);
+print_r($visited);
+//echo "<br><br>";
+$result = array_unique($visited);
+//print_r($result);
+echo count($result);

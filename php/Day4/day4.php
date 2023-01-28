8fc996bb43c7eb6137ace45286117b98386d03b5
Samir Omer <samiromer2@yahoo.com>
2023-01-29 00:18:32 +0200

    day 4 2 part done it took hell long

--- php/Day4/day4.php
+++ php/Day4/day4.php
@@ -5,8 +5,10 @@
 $input = 'yzbqklnj';
 $allgood = 0;
 //$i = 0;
-$i = 0;
+//$i = 0;
+$i = 282749;
 //$i = 141991;
+//$i = 746222;
 while ($allgood == 0)
 {
 $input = $input .$i;
@@ -14,6 +16,28 @@ $output = md5($input);
 // echo $output = hash_hmac('md5', $input, $secretKey);
 
 if (strpos($output, '00000') === 0) {
+    $allgood = 1;
+ }
+    echo $i;
+	echo " ";
+    $i++;
+}
+echo $i-1;
+
+$input = 'yzbqklnj';
+$allgood = 0;
+//$i = 0;
+//$i = 0;
+$i = 9962624;
+//$i = 141991;
+//$i = 746222;
+while ($allgood == 0)
+{
+$input = $input .$i;
+$output = md5($input);
+// echo $output = hash_hmac('md5', $input, $secretKey);
+
+if (strpos($output, '000000') === 0) {
     $allgood = 1;
  }
     echo $i;

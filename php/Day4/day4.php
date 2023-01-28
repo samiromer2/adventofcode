16e62cbbeee094f759d2bee4601aef8f278a22f1
Samir Omer <samiromer2@yahoo.com>
2023-01-28 04:54:41 +0200

    running the code to check if it works first time

---
+++ php/Day4/day4.php
@@ -0,0 +1,25 @@
+<?php 
+
+// If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
+
+$input = 'yzbqklnj';
+$allgood = 0;
+//$i = 0;
+$i = 141992;
+//$i = 141991;
+while ($allgood == 0)
+{
+$input = $input .$i;
+$output = md5($input);
+// echo $output = hash_hmac('md5', $input, $secretKey);
+
+if (strpos($output, '00000') === 0) {
+    $allgood = 1;
+ }
+    echo $i;
+    $i++;
+}
+echo $i-1;
+// echo "<br>";
+
+// echo  dechex($output);
